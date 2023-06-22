c87d8c049e6751527199417a76cd9dc7ea1643f9
Dominik Kučera <kuceradominik123@gmail.com>
2023-06-22 13:53:52 +0200

    creating create-fake-users seeder

---
+++ server/seeders/create-fake-users.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          id: 'c769c38a-9961-4271-aa80-96ea987ab6ca',
+          firstName: 'Admin',
+          lastName: 'Testovací',
+          email: 'test@gmail.com',
+          password:
+            '$2a$12$ZgA6nfSMFKgyZzXdKIYNjuDWeTFQOtRF9VOnKV9NV9xvd55atilqS',
+          createdAt: '2023-06-22T16:30:07.592Z',
+          updatedAt: '2023-06-22T16:30:07.592Z',
+        },
+        {
+          id: 'c976c38a-9961-4271-aa80-96ea987ab6ar',
+          firstName: 'Joe',
+          lastName: 'Rude',
+          email: 'joe@gmail.com',
+          password:
+            '$2a$12$ZgA6nfSMFKgyZzXdKIYNjuDWeTFQOtRF9VOnKV9NV9xvd55atilqS',
+          createdAt: '2023-06-22T16:30:07.592Z',
+          updatedAt: '2023-06-22T16:30:07.592Z',
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
--- server/seeders/users.ts
+++
@@ -1,9 +0,0 @@
-export const users = [
-  {
-    id: 'c769c38a-9961-4271-aa80-96ea987ab6ca',
-    firstName: 'Test',
-    lastName: 'Test',
-    email: 'test@gmail.com',
-    password: '$2a$12$ZgA6nfSMFKgyZzXdKIYNjuDWeTFQOtRF9VOnKV9NV9xvd55atilqS',
-  },
-];
